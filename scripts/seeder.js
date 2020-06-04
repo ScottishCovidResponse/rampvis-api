3f508f6a91a963bd3884ee7a4117c34c90de1ffa
James Scott-Brown <james@jamesscottbrown.com>
2020-06-04 20:41:54 +0100

    automatically seed mongoDB when container created

---
+++ scripts/seeder.js
@@ -0,0 +1,15 @@
+db.users.insert({
+    'name' : 'Admin User',
+    'email' : 'admin@test.com',
+    'createdAt' : '2019-07-25T11:05:28.967Z',
+    'phone' : '123',
+    'role' : 'admin',
+    'password' : '\$2b\$10\$Y/9ddj4mEYwP8hRhphlqPOL7yMKuZB/V5FbDkMCUP.xG9zFF1Hwp2',
+    'address' : {
+        'street' : '100 Street',
+        'city' : 'London',
+        'country' : 'UK',
+        'zip' : 'W0 AB',
+        'alpha2Code': 'GB',
+    }
+})
