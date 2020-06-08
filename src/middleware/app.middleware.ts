ad0356bced4e26d393bcddc451c509f4ebf75282
saifulkhan <saiful.etc@gmail.com>
2020-06-08 16:06:46 +0100

    some changes

--- src/middleware/app.middleware.ts
+++ src/middleware/app.middleware.ts
@@ -33,12 +33,14 @@ function appMiddleware(app: Application) {
 
             // allow requests with no origin, e.g., like mobile apps or curl requests
             if (!origin) {
+                logger.error('appMiddleware: cross origin = ' + origin + ' - allowed');
                 return callback(null, true);
             }
             if (allowedOrigins.indexOf(origin) !== -1) {
+                logger.error('appMiddleware: cross origin = ' + origin + ' - allowed');
                 return callback(null, true);
             } else {
-                logger.error('appMiddleware: cross origin = ' + origin + ' - allowed = ' + allowedOrigins);
+                logger.error('appMiddleware: cross origin = ' + origin + ' - denied. allowedOrigins = ' + allowedOrigins);
                 const msg = 'The CORS policy for this site does not allow access from the specified origin.';
                 return callback(new Error(msg), false);
             }
