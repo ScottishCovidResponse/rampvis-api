ca1f0e5389515782e15fb521550f69df104d3d14
Saiful Khan <saiful.etc@gmail.com>
2020-06-16 23:10:56 +0100

    cors flask server

--- stat-api/controllers/__init__.py
+++ stat-api/controllers/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
+
 from controllers.scotland.region import region
 
 app = Flask(__name__, instance_relative_config=True)
-
+CORS(app)
 
 app.register_blueprint(region, url_prefix='/stat/v1/scotland/region')
