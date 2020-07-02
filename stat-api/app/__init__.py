b25446b69ded8fb36aa1984487ca6b7ce79dae33
saifulkhan <saiful.etc@gmail.com>
2020-07-02 19:37:16 +0100

    add test job schedular

--- stat-api/app/__init__.py
+++ stat-api/app/__init__.py
@@ -2,8 +2,9 @@ from flask import Flask, url_for
 from importlib import import_module
 from logging import basicConfig, DEBUG, getLogger, StreamHandler
 
+
 def register_blueprints(app):
-    for module_name in ('correlation', 'scotland', 'dynamic_data_fetch'):
+    for module_name in ('correlation', 'scotland', 'data_fetch'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
