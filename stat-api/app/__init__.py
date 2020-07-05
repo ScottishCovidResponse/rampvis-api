52c6faf7a4f83e3c2878c2a8314aa9973ea27d31
phongvis <p.nguyen@city.ac.uk>
2020-07-05 22:11:12 +0100

    Simulate stream data

--- stat-api/app/__init__.py
+++ stat-api/app/__init__.py
@@ -4,7 +4,7 @@ from logging import basicConfig, DEBUG, getLogger, StreamHandler
 
 
 def register_blueprints(app):
-    for module_name in ('correlation', 'scotland', 'process_data'):
+    for module_name in ('correlation', 'scotland', 'process_data', 'stream_data'):
         module = import_module('app.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
 
