0d67859e07b67db98496776d3e596214eb180a11
saifulkhan <saiful.etc@gmail.com>
2020-07-09 21:56:19 +0100

    update first 1st batch of APIs

--- data-api/src/controllers/scotland/nhsboard/scotland.nhsboard.controller.ts
+++ data-api/src/controllers/scotland/nhsboard/scotland.nhsboard.controller.ts
@@ -2,17 +2,53 @@ import {NextFunction} from 'connect';
 import {Response} from 'express-serve-static-core';
 import {controller, httpGet} from 'inversify-express-utils';
 
-import { CsvParseError } from '../../../exceptions/exception';
-import { logger } from '../../../utils/logger';
-import { RequestWithUser } from '../../request-with-user.interface';
-import { ScotlandRegionNhs } from './scotland-regions-nhs.enum';
-import { readCSV } from '../../../services/csv.service';
+import {CsvParseError, InvalidQueryParametersException} from '../../../exceptions/exception';
+import {logger} from '../../../utils/logger';
+import {RequestWithUser} from '../../request-with-user.interface';
+import {ScotlandRegionNhs} from './scotland-regions-nhs.enum';
+import {readCSV} from '../../../services/csv.service';
 
 @controller('/scotland')
 export class ScotlandNhsboardController {
 
-    constructor() {}
+    constructor() {
+    }
 
+    //
+    // api/v1/scotland/?table=<>
+    // api/v1/scotland/?table=<>&region=<>
+    //
+    @httpGet('/')
+    public async getTable(request: RequestWithUser, response: Response, next: NextFunction): Promise<void> {
+        const table = request.query.table as string;
+        const region = request.query.region as string;
+        console.log('table = ', table, ', region = ', region);
+
+        if (!table && !region) {
+            return next(new InvalidQueryParametersException('Empty table and region'));
+        }
+
+        try {
+            const data: any[] = await readCSV(table + '.csv');
+
+            if (request.query.region) {
+                response.status(200).send(data.map((d: any) => {
+                    return {
+                        date: d.date,
+                        value: parseInt(d[region], 10),
+                    };
+                }));
+            } else {
+                response.status(200).send(data);
+            }
+        } catch (error) {
+            next(new CsvParseError(500, error.message));
+        }
+    }
+
+
+    // TODO
+    // Deprecate
     @httpGet('/cumulative')
     public async getAllCumulative(request: RequestWithUser, response: Response, next: NextFunction): Promise<void> {
         logger.debug('ScotlandNhsboardController: getAllCumulative:');
