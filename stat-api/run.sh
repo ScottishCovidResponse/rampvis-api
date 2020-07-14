bcb565dd61369b98e483f066ed724168a95beb9a
saifulkhan <saiful.etc@gmail.com>
2020-07-14 13:56:43 +0100

    refactor

--- stat-api/run.sh
+++ stat-api/run.sh
@@ -2,4 +2,4 @@ export FLASK_APP=run.py
 # Set up the DEBUG environment
 export FLASK_ENV=development
 # Start the application (development mode)
-flask run --host=0.0.0.0 --port=5000
+flask run --host=0.0.0.0 --port=3000
