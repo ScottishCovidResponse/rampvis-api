397c7beaa701082daa40a50ad1717075add9b4b5
phongvis <p.nguyen@city.ac.uk>
2020-09-23 22:33:28 +0100

    add script to downloa data product

---
+++ stat-api/download.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+
+import h5py
+import pandas as pd
+import numpy as np
+
+from data_pipeline_api.registry.downloader import Downloader
+
+def generate_components(f):
+    components = []
+    for k in f:
+        one_level = all(isinstance(f[k][k2], h5py.Dataset) for k2 in f[k])
+        if one_level:
+            components.append(k)
+        else:
+            # Assume that if it's not one level, it's two level
+            for k2 in f[k]:
+                components.append(k + '/' + k2)
+    return components
+
+def to_df(f, key):
+    dates = [d.decode() for d in f[key]['Dimension_2_names']]
+    columns = [d.decode() for d in f[key]['Dimension_1_names']]
+    values = np.array(f[key]['array'])
+    df = pd.DataFrame(data=values, index=dates, columns=columns, dtype='Int64')
+    df.index.name = 'date'
+    return df
+
+def process_h5(path):
+    f = h5py.File(path, 'r')
+    components = generate_components(f)
+    for c in components:
+        df = to_df(f, c)
+        folder = Path(os.path.dirname(path))
+        filename = c.replace('/', '--') + '.csv'
+        df.to_csv(folder/filename)
+    
+def download_to_csvs(product_name):
+    "Download the latest file of a data product, convert h5 to csv and save it."
+    downloader = Downloader(data_directory='data')
+    downloader.add_data_product(namespace='SCRC', data_product=product_name)
+    downloader.download()
+
+    folder = Path('data')/product_name
+    folder = folder/max(os.listdir(folder))
+    filename = os.listdir(folder)[0]
+    process_h5(folder/filename)
+
+if __name__ == '__main__':
+    download_to_csvs('records/SARS-CoV-2/scotland/cases-and-management/testing')
