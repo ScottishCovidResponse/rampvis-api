a69b7db2b5e636295d7ed376f3681340f539a66c
Saiful Khan <saiful.etc@gmail.com>
2020-11-10 19:15:09 +0000

    comment out graph database code

--- stat-api/app/controllers/__init__.py
+++ stat-api/app/controllers/__init__.py
@@ -2,4 +2,4 @@ from .stream_data_controller import stream_data_bp
 from .scotland_controller import scotland_bp
 from .process_data_controller import process_data_bp
 from .correlation_controller import correlation_bp
-from .graph_controller import graph_bp
+# from .graph_controller import graph_bp
--- stat-api/app/services/__init__.py
+++ stat-api/app/services/__init__.py
@@ -1,2 +1,2 @@
 from .download_service import download_to_csvs
-from .graph_service import GraphService
+# from .graph_service import GraphService
