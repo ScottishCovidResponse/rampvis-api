3122c01b13ce1f79c56966928df22a4e4d7b42f9
Saiful Khan <saiful.etc@gmail.com>
2020-11-26 12:30:39 +0000

    bug fix for automaper

--- data-api/src/infrastructure/onto-data/onto-data.dto.ts
+++ data-api/src/infrastructure/onto-data/onto-data.dto.ts
@@ -10,9 +10,9 @@ export class OntoDataDto {
     public urlCode: string = undefined as any;
     public endpoint: string = undefined as any;
     public dataType: DATA_TYPE = undefined as any;
-    public source?: SOURCE;
-    public model?: MODEL;
-    public analytics?: ANALYTICS;
+    public source?: SOURCE = undefined as any;
+    public model?: MODEL = undefined as any;
+    public analytics?: ANALYTICS = undefined as any;
     public description: string = undefined as any;
-    public queryParams?:  QueryParamsDto[];
+    public queryParams?: QueryParamsDto[] = undefined as any;
 }
