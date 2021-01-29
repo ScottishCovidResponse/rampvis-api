248d89834371947be64667ebd2e93abc3621f7f8
Saiful Khan <saiful.etc@gmail.com>
2021-01-29 19:12:20 +0000

    send onto page data to flask-ui

--- data-api/src/controllers/ontology/template.controller.ts
+++ data-api/src/controllers/ontology/template.controller.ts
@@ -6,15 +6,21 @@ import { Request, Response } from 'express-serve-static-core';
 import { TYPES } from '../../services/config/types';
 import { OntoPageService } from '../../services/onto-page.service';
 import { logger } from '../../utils/logger';
-import { IPageTemplate } from '../../infrastructure/onto-page/page-template.interface';
 import { SomethingWentWrong } from '../../exceptions/exception';
 import { TemplateService } from '../../services/template.service';
 import { IOntoPage, BINDING_TYPE } from '../../infrastructure/onto-page/onto-page.interface';
-import { OntoPageDto } from '../../infrastructure/onto-page/onto-page.dto';
+import { OntoPageDto, OntoPageExtDto } from '../../infrastructure/onto-page/onto-page.dto';
 import { MAPPING_TYPES } from '../../services/config/automapper.config';
 import { queryParamValidate } from '../../middleware/validators';
 import { OntoPageFilterVm } from '../../infrastructure/onto-page/onto-page-filter.vm';
 import { PaginationVm } from '../../infrastructure/pagination.vm';
+import { BindingDto, BindingExtDto } from '../../infrastructure/onto-page/binding.dto';
+import { OntoVisDto } from '../../infrastructure/onto-vis/onto-vis.dto';
+import { IOntoVis } from '../../infrastructure/onto-vis/onto-vis.interface';
+import { OntoDataDto } from '../../infrastructure/onto-data/onto-data.dto';
+import { IOntoData } from '../../infrastructure/onto-data/onto-data.interface';
+import { OntoDataService } from '../../services/onto-data.service';
+import { OntoVisService } from '../../services/onto-vis.service';
 
 //
 // Un-guarded - only support GET
@@ -24,7 +30,9 @@ import { PaginationVm } from '../../infrastructure/pagination.vm';
 export class TemplateController {
     constructor(
         @inject(TYPES.TemplateService) private templateService: TemplateService,
-        @inject(TYPES.OntoPageService) private ontologyPageService: OntoPageService,
+        @inject(TYPES.OntoVisService) private ontoVisService: OntoVisService,
+        @inject(TYPES.OntoDataService) private ontoDataService: OntoDataService,
+        @inject(TYPES.OntoPageService) private ontoPageService: OntoPageService,
     ) {}
 
     @httpGet('/pages', queryParamValidate(OntoPageFilterVm))
@@ -33,7 +41,7 @@ export class TemplateController {
         logger.info(`TemplateController:getPages: query = ${JSON.stringify(query)}`);
 
         try {
-            const result: PaginationVm<IOntoPage> = await this.ontologyPageService.getAllPages(query);
+            const result: PaginationVm<IOntoPage> = await this.ontoPageService.getAllPages(query);
 
             const resultDto: PaginationVm<OntoPageDto> = {
                 data: automapper.map(MAPPING_TYPES.IOntoPage, MAPPING_TYPES.OntoPageDto, result.data),
@@ -56,13 +64,42 @@ export class TemplateController {
         const pageId: string = request.params.pageId;
         logger.info(`TemplateController:getPageTemplate: pageId = ${pageId}`);
 
+        // Get all pages
+        const ontoPage: IOntoPage = await this.ontoPageService.get(pageId);
+
+        // IOntoPage -> OntoPageDto
+        const ontoPageDto: OntoPageDto = automapper.map( MAPPING_TYPES.IOntoPage, MAPPING_TYPES.OntoPageDto, ontoPage);
+
+        // OntoPageDto -> OntoPageExtDto
+        let bindingExts: BindingExtDto[] = await this.bindingDtoToBindingExtDto(ontoPageDto.bindings);
+        let ontoPageExtDto: OntoPageExtDto = { ...ontoPageDto, bindingExts: bindingExts };
+
         try {
-            const pageTemplate: IPageTemplate = await this.templateService.buildPageTemplate(pageId);
-            logger.info(`TemplateController:getPageTemplate: pageTemplate = ${JSON.stringify(pageTemplate)}`);
-            response.status(200).send(pageTemplate);
+            logger.info(`TemplateController:getPageTemplate: ontoPageExtDto = ${JSON.stringify(ontoPageExtDto)}`);
+            response.status(200).send(ontoPageExtDto);
         } catch (e) {
             logger.error(`TemplateController: getPageTemplate: error = ${JSON.stringify(e)}`);
             next(new SomethingWentWrong(e.message));
         }
+    }
+
+    private async bindingDtoToBindingExtDto(bindings: BindingDto[]) {
+        let bindingExts: BindingExtDto[] = [];
+
+        for (let d of bindings) {
+            const ontoVis: IOntoVis = await this.ontoVisService.get(d.visId);
+            const ontoVisDto: OntoVisDto = automapper.map(MAPPING_TYPES.IOntoVis, MAPPING_TYPES.OntoVisDto, ontoVis);
+
+            let ontoDataDtos: OntoDataDto[] = [];
+            for (let dataId of d.dataIds) {
+                const ontoData: IOntoData = await this.ontoDataService.get(dataId);
+                let ontoDataDto: OntoDataDto = automapper.map(MAPPING_TYPES.IOntoData, MAPPING_TYPES.OntoDataDto, ontoData);
+                ontoDataDtos.push(ontoDataDto);
+            }
+
+            bindingExts.push({ vis: ontoVisDto, data: ontoDataDtos } as BindingExtDto);
+        }
+
+        return bindingExts;
     }
 }
