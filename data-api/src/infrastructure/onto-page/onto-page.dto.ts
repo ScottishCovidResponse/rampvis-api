e7287311393ac1c2c906b9529c22be441f4704d2
Saiful Khan <saiful.khan@eng.ox.ac.uk>
2021-04-15 15:39:12 +0100

    page list should contain binding and bindingext

--- data-api/src/infrastructure/onto-page/onto-page.dto.ts
+++ data-api/src/infrastructure/onto-page/onto-page.dto.ts
@@ -16,5 +16,6 @@ export class OntoPageExtDto {
     public date: Date = undefined as any;
     public nrows: number = undefined as any;
 
+    public bindings?: BindingDto[] = undefined as any;
     public bindingExts: BindingExtDto[] = undefined as any;
 }
