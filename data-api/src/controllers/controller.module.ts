2c996aeb1c1363e81c82634043847a4cf84ed7c3
Saiful Khan <saiful.etc@gmail.com>
2021-09-04 20:39:18 +0100

    remove old data controllers

--- data-api/src/controllers/controller.module.ts
+++ data-api/src/controllers/controller.module.ts
@@ -2,13 +2,7 @@ import { AuthController } from './auth/auth.controller';
 import { MeController } from './user/me.controller';
 import { UserController } from './user/user.controller';
 import { ActivityControllerInt } from './activity.controller';
-import { NhsBoardController } from './scotland/nhs-board.controller';
-import { NhsBoardDynamicController } from './scotland/nhs-board.controller-dynamic';
-import { CovidDeathsController } from './scotland/covid-deaths.controller';
-import { SearchControllerV05 } from './search.controller.v0.5';
 import { ThumbnailController } from './thumbnail.controller';
-import { ScotlandLiveController } from './scotland/scotland-live.controller';
-import { ScotlandCovidModelController } from './scotland/covid-model.controller';
 import { TemplateController } from './ontology/template.controller';
 import { OntoVisController } from './ontology/onto-vis.controller';
 import { OntoDataController } from './ontology/onto-data.controller';
@@ -19,13 +13,7 @@ export {
     MeController,
     UserController,
     ActivityControllerInt,
-    SearchControllerV05,
     ThumbnailController,
-    NhsBoardController,
-    NhsBoardDynamicController,
-    CovidDeathsController,
-    ScotlandLiveController,
-    ScotlandCovidModelController,
     OntoVisController,
     OntoDataController,
     OntoPageController,
