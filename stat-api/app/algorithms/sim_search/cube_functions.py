fd1ba009bae1af7b4332e590811f607383561283
tunagonen <tunagonen@tunagonens-Mac-mini.local>
2021-09-13 16:18:38 +0100

    to_cube functions first draft

---
+++ stat-api/app/algorithms/sim_search/cube_functions.py
@@ -0,0 +1,51 @@
+import pandas
+import datetime
+# data transformations functions
+def country_filter(country_name,df):    # group by country name
+    count_index = df.groupby('location').groups[country_name].values
+    country_data = df.loc[count_index].copy(deep = True)
+    country_data.set_index('date',inplace = True)
+    country_data.drop(['iso_code','population','continent','population_density'],axis=1,inplace=True)
+    
+    # adding derived data streams for each country
+    country_data["weekly_cases"] = country_data["new_cases"].rolling(7).sum()
+    country_data["biweekly_cases"] = country_data["new_cases"].rolling(14).sum()
+    country_data["weekly_cases_per_million"] = country_data["new_cases_per_million"].rolling(7).sum()
+    country_data["biweekly_cases_per_million"] = country_data["new_cases_per_million"].rolling(14).sum()
+    country_data["weekly_cases_rate"] = country_data["weekly_cases"].diff(periods=7) / country_data["weekly_cases"] * 100
+    country_data["biweekly_cases_rate"] = country_data["biweekly_cases"].diff(periods=14) / country_data["biweekly_cases"] * 100
+    
+    country_data["weekly_deaths"] = country_data["new_deaths"].rolling(7).sum()
+    country_data["biweekly_deaths"] = country_data["new_deaths"].rolling(14).sum()
+    country_data["weekly_deaths_per_million"] = country_data["new_deaths_per_million"].rolling(7).sum()
+    country_data["biweekly_deaths_per_million"] = country_data["new_deaths_per_million"].rolling(14).sum()
+    country_data["weekly_deaths_rate"] = country_data["weekly_deaths"].diff(periods=7) / country_data["weekly_deaths"] * 100
+    country_data["biweekly_deaths_rate"] = country_data["biweekly_deaths"].diff(periods=14) / country_data["biweekly_deaths"] * 100
+    country_data.fillna(0,inplace=True)
+    return country_data  
+
+def cube_sample(country,df): # change OWID structure from cube for a single country
+    test_df = country_filter(country,df)
+    country_name = test_df['location'][0]
+    test_df.drop(['location'],axis=1,inplace=True)
+    test_df = test_df[test_df.index > datetime.datetime.strptime("2021-01-01","%Y-%m-%d")]
+    master_lst = [] 
+    temp_dict = {}
+
+    for j in range(len(test_df.index)):
+        for i in range(len(test_df.columns)):
+            temp_dict[test_df.columns[i]] = test_df.iloc[j][i]
+        master_lst.append(temp_dict)
+        temp_dict = {}
+    test = {country_name : master_lst}
+    sample_df = pd.DataFrame(index=test_df.index,data=test)
+    return sample_df
+
+def cube_master(df): # concatanate all transformed countries
+    country_list = df['location'].unique()
+    df_lst = []
+    for country in country_list:
+        
+        df_lst.append(cube_sample(country,df))
+        
+    return pd.concat(df_lst,axis=1)
--- stat-api/app/controllers/timeseries_sim_search_controller.py
+++ stat-api/app/controllers/timeseries_sim_search_controller.py
@@ -9,23 +9,34 @@ import json
 from app.core.settings import DATA_PATH_LIVE
 from app.algorithms.sim_search.precomputation import to_cube
 from app.utils.jwt_service import validate_user_token
+from pydantic import BaseModel
+from typing import List,Dict
+from datetime import date
 
 timeseries_sim_search_controller = APIRouter()
 
+class FirstRunForm(BaseModel):
+    targetCountry: str 
+    firstDate: date
+    lastDate: date
+    indicator: str
+    method: str
+    numberOfResults: int
+    minPopulation: int
+    startDate: date
+    endDate: date
+    continentCheck: Dict[str,bool]
+
+
+@timeseries_sim_search_controller.post("/firstRunForm")
+async def createform(firstRunForm:FirstRunForm):
+    delta = (firstRunForm.lastDate - firstRunForm.firstDate)
+    timeSeriesLength = delta.days
+    #return firstRunForm
+    return delta.days
+
+
 @timeseries_sim_search_controller.get("/")
-async def search(start_date=Query(None),country = Query(None),end_date=Query(None),indicator=Query(None),method=Query(None),result_number=Query(None),q2=Query(None)):
-    logger.info(f"timeseries_sim_search_controller start_date = {start_date}, country = {country}, end_date={end_date},indicator={indicator},method={method},result_number={result_number},q2={q2}")
-
-    # TODO: Just hard code to always return nhs_health_board_date_covid19_patients_in_hospital_confirmed_normalized
-    folder = Path(DATA_PATH_LIVE)
-    product = 'records/SARS-CoV-2/scotland/cases-and-management/hospital'
-    component = 'nhs_health_board_date_covid19_patients_in_hospital_confirmed_normalized'
-    filename = folder/product/(component + '.csv')
-    df = pd.read_csv(filename)
-    result = df.to_json(orient="records")
-    return Response(content=result, media_type="application/json")
-
-
 def precompute():
     """Run any kind of precomputation that is slow for real-time search.
     """
@@ -44,7 +55,7 @@ scheduler.add_job(precompute, "cron", hour=1, minute=0, second=0)
 
 scheduler.start()
 
-@timeseries_sim_search_controller.get("/start", dependencies=[Depends(validate_user_token)])
+@timeseries_sim_search_controller.get("/start")
 def start():
     # Start if hasn't
     if scheduler.state == STATE_STOPPED:
@@ -70,7 +81,7 @@ def start():
         )
 
 
-@timeseries_sim_search_controller.get("/status", dependencies=[Depends(validate_user_token)])
+@timeseries_sim_search_controller.get("/status")
 def status():
     if scheduler.state == STATE_STOPPED:
         return Response(
@@ -91,7 +102,7 @@ def status():
         )
 
 
-@timeseries_sim_search_controller.get("/stop", dependencies=[Depends(validate_user_token)])
+@timeseries_sim_search_controller.get("/stop")
 def stop():
     scheduler.pause()
     return Response(
