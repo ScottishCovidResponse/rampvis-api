458b75005e161aed0a34562723ab337a8e479c73
Erik <supersamst.rydow@gmail.com>
2021-11-04 17:38:46 +0000

    Using JSON import data

--- processing-api/app/controllers/sensitivity_analysis_agent.py
+++ processing-api/app/controllers/sensitivity_analysis_agent.py
@@ -2,7 +2,11 @@ from pathlib import Path
 
 from loguru import logger
 import pandas as pd
+import json
 from apscheduler.schedulers.background import BackgroundScheduler
+
+from sandu.data_types import SensitivityInput
+from sandu.sensitivity_analysis import sobol
 
 from app.core.settings import DATA_PATH_LIVE
 
@@ -10,8 +14,15 @@ def convert_data():
     """Convert raw data to the format required by sensitivity analysis.
     """
     folder = Path(DATA_PATH_LIVE)
-    df = pd.read_csv(folder/"models/sobol/raw.csv")
-    df.to_csv(folder/"models/sobol/processed.csv", index=False)
+    
+    with open(folder/"models/sobol/raw.json", "r") as read_file:
+        x = json.load(read_file, object_hook=lambda d: SensitivityInput(**d))
+
+    N = 2 ** 12  # Note: Must be a power of 2.  (N*(2D+2) parameter value samples are used in the Sobol analysis.
+
+    Si_df = sobol.get_indices(x.df(), x.parameters, x.bounds, x.quantity_mean, x.quantity_variance, N)  # Perform analysis
+    Si_df.index.name = "Parameter"
+    Si_df.to_csv(folder/"models/sobol/processed.csv", index=False)
 
 # A recurrent job
 scheduler = BackgroundScheduler(daemon=True)
