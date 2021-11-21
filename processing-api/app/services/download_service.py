db37e78935884e0f2e1f962bd73bc2af68cfe414
Erik <supersamst.rydow@gmail.com>
2021-11-21 10:19:39 +0000

    Extracts zip contents directly to save_to

--- processing-api/app/services/download_service.py
+++ processing-api/app/services/download_service.py
@@ -134,7 +134,8 @@ def download_open_data(folder):
 def download_urls(urls, folder):
     folder = Path(folder)
     for url in urls:
-        Path(folder/url['save_to']).parents[0].mkdir(parents=True, exist_ok=True) #Create directory for file if not already present
+        parentfolder = Path(folder/url['save_to']).parents[0]
+        parentfolder.mkdir(parents=True, exist_ok=True)  # Create directory for file if not already present
         if url['url'].lower().endswith('.csv'):
             df = pd.read_csv(url['url'])
             df.to_csv(folder/url['save_to'], index=None)
@@ -145,4 +146,9 @@ def download_urls(urls, folder):
         elif url['url'].lower().endswith('.zip'): #download and unzip zip files
             http_response = urlopen(url['url'])
             zipfile = ZipFile(BytesIO(http_response.read()))
-            zipfile.extractall(path=folder/url['save_to'])
+            zipinfos = zipfile.infolist()
+            # iterate through each file and remove the top directory
+            for zipinfo in zipinfos:
+                zipinfo.filename = zipinfo.filename.removeprefix(zipinfo.filename.split('/')[0])  #Removes the top level folder when extracting
+                zipfile.extract(zipinfo, path=folder/url['save_to'])
+
