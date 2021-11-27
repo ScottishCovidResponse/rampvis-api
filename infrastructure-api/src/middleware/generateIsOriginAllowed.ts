2172b18a7f2f345a461f74b5f70cbdf00ba950c6
Alexander Kachkaev <alexander@kachkaev.ru>
2021-11-27 16:17:07 +0000

    Improve origin matching in CORS (#27)
    * Improve origin matching in CORS
    
    * Add comment
    
    * Add missing continue
    
    * Improve readibilty

---
+++ infrastructure-api/src/middleware/generateIsOriginAllowed.ts
@@ -0,0 +1,111 @@
+import { Logger } from 'winston';
+
+type IsOriginAllowed = (origin: string | undefined) => boolean;
+
+/**
+ * This function accepts a list of allowed origins (hosts) and returns a function
+ * that can check a given origin.
+ *
+ * Wildcard matching is supported for origin endings.
+ *
+ * Example values:
+ *     ✅ example.com
+ *     ✅ demo.example.com
+ *     ✅ *.example.com
+ *     ✅ *--demo.example.com
+ *     ✅ *example.com
+ *     ✅ 42.42.42.42
+ *
+ * Invalid values in the list of allowedOrigins are ignored with a warning.
+ *     🗑 ExAmple.COM
+ *     🗑 example.*
+ *     🗑     example  .  com
+ *     🗑 demo.*.example.com
+ *     🗑 42 (number)
+ *     🗑 null
+ *     🗑 {}
+ */
+export function generateIsOriginAllowed(allowedOrigins: unknown, logger?: Logger): IsOriginAllowed {
+    const allowedOriginLookup: Record<string, true> = {};
+    const allowedOriginEndingLookup: Record<string, true> = {};
+
+    if (!Array.isArray(allowedOrigins)) {
+        logger?.warn('Expected allowedOrigins to be an array. Allowing all origins.');
+        return () => true;
+    }
+
+    for (const allowedOrigin of allowedOrigins) {
+        if (typeof allowedOrigin !== 'string') {
+            logger?.warn(`Ignoring allowedOrigins → ${allowedOrigin} (expected a string)`);
+            continue;
+        }
+
+        const normalisedAllowedOrigin = allowedOrigin.replace(/\s/g, '').toLowerCase();
+        if (!normalisedAllowedOrigin.length) {
+            logger?.warn(`Ignoring allowedOrigins → "${allowedOrigin}" (expected a non-empty string)`);
+            continue;
+        }
+
+        if (normalisedAllowedOrigin !== allowedOrigin) {
+            logger?.warn(
+                `Ignoring allowedOrigins → "${allowedOrigin}" (expected a normalised string: "${normalisedAllowedOrigin}")`
+            );
+            continue;
+        }
+
+        // TODO: improve host matching if needed, e.g. by using
+        // https://github.com/validatorjs/validator.js/blob/master/src/lib/isFQDN.js
+
+        if (normalisedAllowedOrigin.includes('*')) {
+            if (normalisedAllowedOrigin.lastIndexOf('*') !== 0) {
+                logger?.warn(
+                    `Ignoring allowedOrigins → "${allowedOrigin}" (only patterns like "*.example.com" or "*--demo.example.com" are supported)`
+                );
+                continue;
+            }
+
+            const normalisedAllowedOriginEnding = normalisedAllowedOrigin.substring(1);
+            if (normalisedAllowedOrigin) {
+                logger?.warn(`Duplicate value in allowedOrigins: "${allowedOrigin}"`);
+            } else {
+                allowedOriginEndingLookup[normalisedAllowedOriginEnding] = true;
+            }
+            continue;
+        }
+
+        if (allowedOriginLookup[normalisedAllowedOrigin]) {
+            logger?.warn(`Duplicate value in allowedOrigins: "${allowedOrigin}"`);
+        } else {
+            allowedOriginLookup[normalisedAllowedOrigin] = true;
+        }
+    }
+
+    const allowedOriginEndings = Object.keys(allowedOriginEndingLookup);
+
+    if (!allowedOriginEndings.length && !Object.keys(allowedOriginLookup).length) {
+        logger?.warn('No valid values found in allowedOrigins. Allowing all origins.');
+        return () => true;
+    }
+
+    return (origin) => {
+        // Allow requests with no origin, e.g., like mobile apps or curl requests
+        if (!origin) {
+            return true;
+        }
+
+        // Exact match
+        if (allowedOriginLookup[origin]) {
+            return true;
+        }
+
+        // Ending match
+        for (const allowedOriginEnding of allowedOriginEndings) {
+            if (origin.endsWith(allowedOriginEnding)) {
+                return true;
+            }
+        }
+
+        // No match
+        return false;
+    };
+}
