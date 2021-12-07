8002bc0832c9a8daf43af0cbff79afdafeb6074a
saiful.khan <saiful.etc@gmail.com>
2021-12-07 11:55:27 +0000

    minor refactor

--- data-api/app/main.py
+++ data-api/app/main.py
@@ -9,26 +9,24 @@ from app.core.settings import GLOBAL_CONFIG_OBJ
 from app.core.settings import DEFAULT_ROUTE_STR
 from app.controllers import router
 
+logger.info(f"PROJECT_NAME = {PROJECT_NAME}")
+logger.debug(f"GLOBAL_CONFIG_OBJ = {GLOBAL_CONFIG_OBJ}")
 
 app = FastAPI(title=PROJECT_NAME)
 app.add_middleware(GZipMiddleware, minimum_size=1000)
 
-logger.info(f"GLOBAL_CONFIG_OBJ = {GLOBAL_CONFIG_OBJ}")
-
-app.add_middleware(CORSMiddleware, 
-        allow_origin_regex=GLOBAL_CONFIG_OBJ["allowOriginRegex"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origin_regex=GLOBAL_CONFIG_OBJ["allowOriginRegex"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router, prefix=DEFAULT_ROUTE_STR)
 
 
 @app.get("/stat/v1/ping")
 def ping():
-    logger.info(
-        f"main:ping: PROJECT_NAME = {PROJECT_NAME}, GLOBAL_CONFIG_OBJ = {GLOBAL_CONFIG_OBJ}"
-    )
     return {"ping": "live"}
 
 
