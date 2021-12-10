d5606edf1e97d4208d5f8231bd6de78fab50466b
phongvis <phonghainguyen84@gmail.com>
2021-12-10 18:04:36 +0000

    no need to have endpoint for timeseries scheduler

--- data-api/app/controllers/timeseries_sim_search_controller.py
+++ data-api/app/controllers/timeseries_sim_search_controller.py
@@ -3,8 +3,6 @@ import threading
 
 from loguru import logger
 from fastapi import APIRouter, Query, Response, Depends
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.schedulers.base import STATE_STOPPED, STATE_RUNNING, STATE_PAUSED
 import pandas as pd
 import json
 from app.core.settings import DATA_PATH_LIVE
@@ -49,59 +47,3 @@ async def createform(firstRunForm:FirstRunForm):
     continentCheck = continentTransformer(continentCheck)
     out = firstRunOutput(cube,targetCountry,firstDate,lastDate,indicator,method,numberOfResults,minPopulation,startDate,endDate,continentCheck)
     return out
-
-
-@timeseries_sim_search_controller.get("/start")
-def start():
-    # Start if hasn't
-    if scheduler.state == STATE_STOPPED:
-        scheduler.start()
-        return Response(
-            json.dumps({"message": "Timeseries sim search: precomputation has started."}),
-            media_type="application/json",
-        )
-
-    # Resume if paused
-    if scheduler.state == STATE_PAUSED:
-        scheduler.resume()
-        return Response(
-            json.dumps({"message": "Timeseries sim search: precomputation has started."}),
-            media_type="application/json",
-        )
-
-    # Already running
-    if scheduler.state == STATE_RUNNING:
-        return Response(
-            json.dumps({"message": "Timeseries sim search: precomputation is already running."}),
-            media_type="application/json",
-        )
-
-
-@timeseries_sim_search_controller.get("/status")
-def status():
-    if scheduler.state == STATE_STOPPED:
-        return Response(
-            json.dumps({"message": "Timeseries sim search: precomputation has not started."}),
-            media_type="application/json",
-        )
-
-    if scheduler.state == STATE_PAUSED:
-        return Response(
-            json.dumps({"message": "Timeseries sim search: precomputation has paused."}),
-            media_type="application/json",
-        )
-
-    if scheduler.state == STATE_RUNNING:
-        return Response(
-            json.dumps({"message": "Timeseries sim search: precomputation is running."}),
-            media_type="application/json",
-        )
-
-
-@timeseries_sim_search_controller.get("/stop")
-def stop():
-    scheduler.pause()
-    return Response(
-        json.dumps({"message": "Timeseries sim search: precomputation has stopped."}),
-        media_type="application/json",
-    )
