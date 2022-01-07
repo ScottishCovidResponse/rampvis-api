3ca0cd0914b7b859d1c6f1af35d2bd6f6ef8ec83
tunagonen <tunagonen@gmail.com>
2022-01-07 14:54:09 +0300

    benchmark compare v1

--- data-api/app/algorithms/sim_search/comparefunction.py
+++ data-api/app/algorithms/sim_search/comparefunction.py
@@ -11,7 +11,7 @@ def compareOutput(benchmarkCountries):
         df_prot.set_index("date",inplace=True)
 
         obj = {}
-        obj["id"] = streams
+        obj["key"] = streams
         df_filt = df_prot.filter(items=countries).tail(30).fillna(0)
         obj["value"] = df_filt.to_dict()
         obj_lst.append(obj)
@@ -19,7 +19,7 @@ def compareOutput(benchmarkCountries):
     df_cat  = pd.read_csv(PATH_SEARCH+"/categorical_variables.csv")
     df_cat_filt = df_cat.loc[df_cat['location'].isin(benchmarkCountries)]
     df_cat_filt.fillna(0,inplace=True)
-    obj_cat["id"] = "categorical_variables"
+    obj_cat["key"] = "categorical_variables"
     inner_obj = {}
     for i in range(len(df_cat_filt)):
         inner_obj[df_cat_filt.iloc[i]["location"]] = df_cat_filt.iloc[0].filter(items=df_cat_filt.columns[df_cat_filt.columns.get_loc("stringency_index"):]).to_dict()
