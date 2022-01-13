62ba9085138211437e13fc5f28ff369c71188ef2
Erik <supersamst.rydow@gmail.com>
2022-01-13 18:53:14 +0000

    Parameter-based Clustering (#36)
    Forms clusters based on the input space, as opposed the the previous clustering based on the output curves.

--- data-api/app/controllers/agents/sensitivity_range_mean_sample_agent.py
+++ data-api/app/controllers/agents/sensitivity_range_mean_sample_agent.py
@@ -7,6 +7,7 @@ import json
 import app.controllers.agents.uncertainty_analysis.clustering_tools as ct
 import app.controllers.agents.sensitivity_analysis.sensitivity_model_inventory as inventory
 
+from sandu.data_types import SensitivityInput
 from app.core.settings import DATA_PATH_LIVE
 
 
@@ -29,7 +30,7 @@ def clustering_range_mean(raw_clusters: List[dict]):
 
 
 def get_cluster_list(model_list: List[dict]) -> List[dict]:
-    """Form a list of dictionaries containing the information needed to compute clusters, based on the information in model_list.
+    """Form a list of dictionaries containing the information needed to compute clusters based in the outputs and inputs of the model.
 
     Args:
         model_list: List containing a dictionary with: the name, k-values, and distance metrics to be used for cluster analysis of model data.
@@ -38,9 +39,12 @@ def get_cluster_list(model_list: List[dict]) -> List[dict]:
         cluster_list: A list containing a dictionary with information on each cluster needed to form the clusters.
 
     """
+    folder = Path(DATA_PATH_LIVE)
     models = model_list
     cluster_list = []
     metric_abbreviation = {"euclidean": "e", "dtw": "dtw"}
+    
+    # output_clustering
     for model in models:
         for i in model["k"]:
             for metric in model["metric"]:
@@ -52,7 +56,26 @@ def get_cluster_list(model_list: List[dict]) -> List[dict]:
                                             metric_abbreviation[metric] + ".json"
                          }
                     )
+    
+    # input clustering
+    for model in models:
+        for i in model["k"]:
+            for metric in model["metric"]:
+                for quantity in model["quantities_of_interest"]:
+                    with open(str(folder) + "/models/sensitivity/" + model["name"] + "/" + quantity["name"] + "_raw.json", "r") as read_file:
+                        x = json.load(read_file, object_hook=lambda d: SensitivityInput(**d))
+                        
+                    parameters = [x.parameters[i] for i in range(len(x.parameters)) if len(x.bounds[i]) > 1]
+                    for parameter in parameters:
+                        cluster_list.append(
+                            {"filename": "models/sensitivity/" + model["name"] + "/" + quantity["name"] + "_raw_k" + str(i) + "_" +
+                                         metric_abbreviation[metric] + "_" + parameter + ".json",
+                             "output_filename": "models/sensitivity/" + model["name"] + "/" + quantity["name"] + "_range_mean_k" + str(i) + "_" +
+                                                metric_abbreviation[metric] + "_" + parameter + ".json"
+                             }
+                        )
     return cluster_list
+
 
 
 def sensitivity_clustering_range_mean_agent():
