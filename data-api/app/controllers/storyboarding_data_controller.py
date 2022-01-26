1576dcc2c42f2be209d52e0e282b86f8edb1b67c
Phong Nguyen <phong.nguyen@redsift.io>
2022-01-26 22:32:39 +0700

    add data back

--- data-api/app/controllers/storyboarding_data_controller.py
+++ data-api/app/controllers/storyboarding_data_controller.py
@@ -12,7 +12,7 @@ storyboarding_data_controller = APIRouter()
 async def get():
     logger.info(f"storyboarding_data_controller:get:")
 
-    filename = Path(DATA_PATH_LIVE) / "phe/ltla/newCasesByPublishDate.csv"
+    filename = Path(DATA_PATH_LIVE) / "phe/ltla/newcasesbypublishdate.csv"
     df = pd.read_csv(filename)
     result = df.to_json(orient="records")
     return Response(content=result, media_type="application/json")
